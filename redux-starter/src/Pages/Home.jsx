df8cb9b075794553fc33bb50c3b0a9807a6a4268
thisis-arman <armanhossen1549@gmail.com>
2023-08-22 11:51:39 +0600

    Folder setup

---
+++ redux-starter/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-extrabold text-center py-10 text-purple-600 underline"> Redux Starter</h1>
+
+        </div>
+    );
+};
+
+export default Home;
