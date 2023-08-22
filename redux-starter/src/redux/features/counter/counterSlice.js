35a036707323da90c4565ae635daa486a08015d6
thisis-arman <armanhossen1549@gmail.com>
2023-08-22 15:27:20 +0600

    store function

---
+++ redux-starter/src/redux/features/counter/counterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    count : 0,
+};
+
+const counterSlice =createSlice({
+    name:'counter',
+    initialState,
+    reducers:{}
+})
+
+export default counterSlice.reducer;
--- redux-starter/src/redux/store.jsx
+++ redux-starter/src/redux/store.jsx
@@ -1,9 +1,12 @@
 
 
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/counter/counterSlice'
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        counter: counterReducer,
+    },
 })
 
 
